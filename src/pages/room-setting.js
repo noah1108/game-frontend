2c470fd5b4afcce7df854a536acd54a1f45f7d84
shimada_hikaru <shimada_hikaru>
2023-06-14 22:10:22 +0900

    add コメント

--- src/pages/room-setting.js
+++ src/pages/room-setting.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Grid, TextField } from '@mui/material'
 import { useContext } from 'react'
 import { SocketContext } from './_app'
@@ -10,9 +10,40 @@ const RoomSetting = () => {
   const [socket, setSocket] = useContext(SocketContext)
   const [username, setUsername] = useState('')
 
+  // ソケットイベント定義
+  useEffect(() => {
+    // 部屋建てボタン押下のレスポンスを受けての処理
+
+    /**
+     * 返却形式
+     *  {
+     *    status:200 or Exxx(エラー)
+     *    message:""
+     *    data:{
+     *      ※コンソールなどで確認
+     *    }
+     *  }
+     */
+
+    // 正常系処理(戻り値=200)
+    // 1. レスポンスから部屋番号を取得
+    // 2. 部屋番号をパラメータで渡し、wait画面へ遷移
+
+    // 異常系処理(戻り値=Exxx)
+    // 1. レスポンスからエラーメッセージを取得
+    // 2. 赤文字でエラーメッセージを表示する
+    // ※ 画面遷移しない
+
+    socket.on('create', (res) => {
+
+    })
+    
+  }, [])
+
   // 部屋を建てるボタン押下イベントハンドラ
   const handleOnClick = () => {
     socket.emit('create', username)
+    // TODO ↓ 要る? 使用感と相談
     setUsername('')
   }
 
