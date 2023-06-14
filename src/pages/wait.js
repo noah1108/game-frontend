e2f3862849a8b1ce723cf3aedb1b89965eefaeb9
shimada_hikaru <shimada_hikaru>
2023-06-15 00:14:23 +0900

    impl wait画面

--- src/pages/wait.js
+++ src/pages/wait.js
@@ -1,9 +1,30 @@
 import { Grid } from '@mui/material'
+import { useRouter } from 'next/router'
+import { useContext, useEffect, useState } from 'react'
+import { SocketContext } from './_app'
+import _ from 'lodash'
 
 /**
  *  参加待ち画面
  */
 const Wait = () => {
+  const [socket, setSocket] = useContext(SocketContext)
+  const [room, setRoom] = useState({})
+  const router = useRouter()
+  // reloadされたら詰む
+  const roomId = router.query.roomId
+  
+  // ソケットイベント定義
+  useEffect(() => {
+    if (!_.isEmpty(socket)) {
+      socket.on('getRoomRes', (res) => {
+        console.log(res)
+      })
+
+      // 部屋情報取得
+      socket.emit('getRoom',roomId)
+    }
+  }, [socket])
 
   return (
     <Grid item>
